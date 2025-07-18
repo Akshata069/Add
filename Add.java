67adee32c97ca628ced009d849da83b18e27f1b4
Akshata069 <akshatabiradar966@gamil.com>
2025-07-18 09:17:15 +0530

    save

---
+++ Add.java
@@ -0,0 +1,11 @@
+package login;
+import java.util.*;
+public class Add {
+    public static  void main(String[] args)
+    {
+        int a=10;
+        int b=5;
+        int c=a+b;
+        System.out.println("sum of two numbers is "+c);
+    }
+}
